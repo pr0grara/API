8e01678799a51d5b7d19aadd59bca301c28b45dd
Ara Z Baghdassarian <azbaghda@gmail.com>
2023-01-19 12:29:19 -0800

    add audio

---
+++ routes/audio.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/:file/:type', (req, res) => {
+    let fileName = req.params.file;
+    let type = req.params.type || "mp3";
+
+    res.set({'Content-Type': `audio/${type}`});
+    res.status(200).sendFile(path.resolve(__dirname, '../', 'assets', 'audio', `${fileName}.${type}`)).end();
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.status(200).send("API HOME").end()
+});
+
+
+
+app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
