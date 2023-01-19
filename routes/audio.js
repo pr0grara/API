ff686e50f6400ff72646ee65a903bb80a562d3df
Ara Z Baghdassarian <azbaghda@gmail.com>
2023-01-19 13:04:17 -0800

    update audio route

--- routes/audio.js
+++ routes/audio.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/:file/:type', (req, res) => {
+router.get('/', (req, res) => {
+    console.log(req.params);
+    res.status(200).send('audio').end();
+});
+
+router.get('/src/:file-:type', (req, res) => {
     let fileName = req.params.file;
     let type = req.params.type || "mp3";
+    let filePath = path.resolve(__dirname, "../", "assets", "audio", `${fileName}.${type}`);
 
     res.set({'Content-Type': `audio/${type}`});
-    res.status(200).sendFile(path.resolve(__dirname, '../', 'assets', 'audio', `${fileName}.${type}`)).end();
+    res.status(200).sendFile(filePath)
 });
 
 module.exports = router;
