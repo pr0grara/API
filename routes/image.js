fafefecfd00697dc75755f984f9cd6104205ffe9
Ara Z Baghdassarian <azbaghda@gmail.com>
2023-01-20 12:37:25 -0800

    add aram bear image and image routes

--- routes/image.js
+++ routes/image.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.status(200).send('audio').end();
+    res.status(200).send('image').end();
 });
 
 router.get('/src/:file-:type', (req, res) => {
     let fileName = req.params.file;
-    let type = req.params.type || "mp3";
-    let filePath = path.resolve(__dirname, "../", "assets", "audio", `${fileName}.${type}`);
+    let type = req.params.type || "png";
+    let filePath = path.resolve(__dirname, "../", "assets", "image", `${fileName}.${type}`);
 
-    res.set({'Content-Type': `audio/${type}`});
+    res.set({'Content-Type': `image/${type}`});
     res.status(200).sendFile(filePath)
 });
 
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const audio = require('./routes/audio');
+const image = require('./routes/image');
 
 app.use('/audio', audio);
+app.use('/image', image);
 
 app.get('/', (req, res) => {
     res.status(200).send("API HOME").end()
