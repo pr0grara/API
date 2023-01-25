2f67333b292aee31ea3233d75da3541d4201c0b4
Ara Z Baghdassarian <azbaghda@gmail.com>
2023-01-25 10:57:49 -0800

    add icon route

---
+++ routes/icon.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.status(200).send('icon').end();
+});
+
+router.get('/src/:file-:type', (req, res) => {
+    let fileName = req.params.file;
+    let type = req.params.type || "svg";
+    let filePath = path.resolve(__dirname, "../", "assets", "icon", `${fileName}.${type}`);
+
+    res.set({ 'Content-Type': `image/${type}` });
+    res.status(200).sendFile(filePath)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const PORT = process.env.PORT || 8080;
 
 const audio = require('./routes/audio');
 const image = require('./routes/image');
+const icon = require('./routes/icon');
 
 app.use('/audio', audio);
 app.use('/image', image);
+app.use('/icon', icon);
 
 app.get('/', (req, res) => {
     res.status(200).send("API HOME").end()
