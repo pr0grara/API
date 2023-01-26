a8b01eb7d78ad4185a97d026c23657d4b593004a
Ara Z Baghdassarian <azbaghda@gmail.com>
2023-01-25 18:51:24 -0800

    update svg option in image and icon routes

--- routes/image.js
+++ routes/image.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
 router.get('/src/:file-:type', (req, res) => {
     let fileName = req.params.file;
     let type = req.params.type || "png";
-    if (type === 'svg') type = type + "+xlm";
     let filePath = path.resolve(__dirname, "../", "assets", "image", `${fileName}.${type}`);
-
+    
+    if (type === 'svg') type = type + "+xlm";
     res.set({'Content-Type': `image/${type}`});
     res.status(200).sendFile(filePath)
 });
